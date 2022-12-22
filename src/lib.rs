8bb60fd12b8c2ed646e45a6b64bcc19a4a7e1d7d
Eslam Hindawy <eslam.foad@gmail.com>
2022-12-23 00:08:09 +0100

    use error_for_status to substitute the Ok(bool)

--- src/lib.rs
+++ src/lib.rs
@@ -11,18 +11,15 @@ pub fn get_hue_hub_ip() -> Option<String> {
         return None;
     }
     let output = Command::new("arp")
-        // .arg("-c")
-        // .arg("arp")
         .arg("-a")
         .output()
         .expect("failed to run `arp -a` command");
     let result = str::from_utf8(&output.stdout);
     let re = Regex::new(r"\((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\)").unwrap();
-    let mut ip = "";
     for cap in re.captures_iter(&result.expect("couldb't run")) {
         match  ping_hue(&cap[1]){
-            Ok(true) => {
-                return Some(String::from(&cap[1].to_string()));
+            Ok(()) => {
+                return Some(String::from(&cap[1]));
             },
             _ => continue
         }
@@ -30,10 +27,10 @@ pub fn get_hue_hub_ip() -> Option<String> {
     None
 }
 
-fn ping_hue(ip: &str) -> Result<bool, Box<dyn std::error::Error>> {
+fn ping_hue(ip: &str) -> Result<(), Box<dyn std::error::Error>> {
     println!("Pinging: {ip} ...");
-    let resp = reqwest::blocking::get(format!("http://{ip}/api/newdeveloper"))?;
-    Ok(resp.status().is_success())
+    reqwest::blocking::get(format!("http://{ip}/api/newdeveloper"))?.error_for_status()?;
+    Ok(())
 }
 
 
